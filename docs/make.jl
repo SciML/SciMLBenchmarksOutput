a5ff626db18633c7dab5a274732d884ce8699dff
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-07-24 16:42:53 -0400

    build all pages

--- docs/make.jl
+++ docs/make.jl
@@ -3,32 +3,23 @@ using Documenter, SciMLBenchmarksOutput
 dir = pkgdir(SciMLBenchmarksOutput)
 cp(joinpath(dir, "markdown"), joinpath(dir, "docs", "src"), force=true)
 cp(joinpath(dir, "README.md"), joinpath(dir, "docs", "src", "index.md"), force=true)
+benchmarksdir = joinpath(dir, "docs", "src")
 
-pages = Any["SciMLBenchmarks" => "index.md"]
-
-for folder in readdir(joinpath(dir, "docs", "src"))
-    @show folder[end-2:end]
-    if folder[end-2:end] != ".md" && folder != "Testing" && folder != "figures"
-        newpages = [joinpath(folder, file) for file in
-                    filter(x -> x[end-2:end] == ".md", readdir(
-                                                    joinpath(dir, "docs", "src", folder)))]
-        push!(pages, [folder => newpages])
-    end
-end
+include("pages.jl")
 
 makedocs(
     sitename="The SciML Benchmarks",
     authors="Chris Rackauckas",
     modules=[SciMLBenchmarksOutput],
-    clean=true,doctest=false,
-    format = Documenter.HTML(#analytics = "UA-90474609-3",
-                             assets = ["assets/favicon.ico"],
-                             canonical="https://benchmarks.sciml.ai/stable/"),
+    clean=true, doctest=false,
+    format=Documenter.HTML(#analytics = "UA-90474609-3",
+        assets=["assets/favicon.ico"],
+        canonical="https://benchmarks.sciml.ai/stable/"),
     pages=pages
 )
 
 deploydocs(;
     repo="github.com/SciML/SciMLBenchmarksOutput",
     devbranch="main",
-    branch = "main"
+    branch="main"
 )
