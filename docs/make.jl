15d039ddc56231dfbd90a6b3e6f46888f9dd2cfb
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-07-24 16:19:42 -0400

    try new docs build

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,30 @@
 using Documenter, SciMLBenchmarksOutput
 
 dir = pkgdir(SciMLBenchmarksOutput)
-cp(joinpath(dir, "README.md"), joinpath(dir, "docs", "src", "index.md"), force=true)
 cp(joinpath(dir, "markdown"), joinpath(dir, "docs", "src"), force=true)
+cp(joinpath(dir, "README.md"), joinpath(dir, "docs", "src", "index.md"), force=true)
+
+pages = Any["SciMLBenchmarks" => "index.md"]
+
+for folder in readdir(joinpath(dir, "docs", "src"))
+    @show folder[end-2:end]
+    if folder[end-2:end] != ".md" && folder != "Testing" && folder != "figures"
+        newpages = [joinpath(folder, file) for file in
+                    filter(x -> x[end-2:end] == ".md", readdir(
+                                                    joinpath(dir, "docs", "src", folder)))]
+        push!(pages, [folder => newpages])
+    end
+end
 
 makedocs(
-    sitename="The SciML Benchmarks: Differential Equations, Inverse Problems, Physics-Informed ML, Science-Guided AI",
+    sitename="The SciML Benchmarks",
     authors="Chris Rackauckas",
     modules=[SciMLBenchmarksOutput],
     clean=true,doctest=false,
     format = Documenter.HTML(#analytics = "UA-90474609-3",
                              assets = ["assets/favicon.ico"],
                              canonical="https://benchmarks.sciml.ai/stable/"),
-    pages=[
-        "Home" => "index.md",
-        "Non-Stiff Ordinary Differential Equation (ODE) Solver Benchmarks" => Any[
-            "markdown/MultiLanguage/wrapper_packages.md"
-        ],
-    ]
+    pages=pages
 )
 
 deploydocs(;
