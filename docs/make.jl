f24335fc7dd733dba26fc3989713afa6a89bab77
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-07-24 20:43:24 -0400

    fix the build

--- docs/make.jl
+++ docs/make.jl
@@ -6,25 +6,24 @@ dir = @__DIR__() * "/.."
 @show readdir(dir)
 
 cp(joinpath(dir, "markdown"), joinpath(dir, "docs", "src"), force=true)
+cp(joinpath(dir, "README.md"), joinpath(dir, "docs", "src", "index.md"), force=true)
+benchmarksdir = joinpath(dir, "docs", "src")
+
+include("pages.jl")
 
 makedocs(
-    sitename="The SciML Benchmarks: Differential Equations, Inverse Problems, Physics-Informed ML, Science-Guided AI",
+    sitename="The SciML Benchmarks",
     authors="Chris Rackauckas",
     modules=[SciMLBenchmarksOutput],
-    clean=true,doctest=false,
-    format = Documenter.HTML(#analytics = "UA-90474609-3",
-                             assets = ["assets/favicon.ico"],
-                             canonical="https://benchmarks.sciml.ai/stable/"),
-    pages=[
-        "Home" => "index.md",
-        "Non-Stiff Ordinary Differential Equation (ODE) Solver Benchmarks" => Any[
-            "markdown/MultiLanguage/wrapper_packages.md"
-        ],
-    ]
+    clean=true, doctest=false,
+    format=Documenter.HTML(#analytics = "UA-90474609-3",
+        assets=["assets/favicon.ico"],
+        canonical="https://benchmarks.sciml.ai/stable/"),
+    pages=pages
 )
 
 deploydocs(;
     repo="github.com/SciML/SciMLBenchmarksOutput",
     devbranch="main",
-    branch = "main"
+    branch="main"
 )
