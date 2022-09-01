dac9fe5fad3f536a925f4c0f4fece6a1f27a7349
SciML Benchmarks CI <buildkite@julialang.org>
2022-09-01 00:41:04 +0000

    Automatic build\nPublished by build of: https://github.com/SciML/SciMLBenchmarks.jl/commit/6f1a64bb4e955e7e5f7d353e06f0a2c941363ad3

---
+++ script/Bio/multistate.jl
@@ -0,0 +1,63 @@
+
+using DiffEqBase, OrdinaryDiffEq, Catalyst, ReactionNetworkImporters,
+      Sundials, Plots, DiffEqDevTools, ODEInterface, ODEInterfaceDiffEq,
+      LSODA, TimerOutputs, LinearAlgebra, ModelingToolkit, BenchmarkTools
+
+gr()
+const to = TimerOutput()
+tf       = 1000.0
+
+# generate ModelingToolkit ODEs
+@timeit to "Parse Network" prnbng = loadrxnetwork(BNGNetwork(), joinpath(@__DIR__, "Models/multistate.net"))
+rn    = prnbng.rn
+@timeit to "Create ODESys" osys = convert(ODESystem, rn)
+
+tspan = (0.,tf)
+@timeit to "ODEProb No Jac" oprob = ODEProblem(osys, Float64[], tspan, Float64[])
+
+
+@show numspecies(rn) # Number of ODEs
+@show numreactions(rn) # Apprx. number of terms in the ODE
+@show length(parameters(rn)) # Number of Parameters
+
+
+u  = ModelingToolkit.varmap_to_vars(nothing, species(rn); defaults=ModelingToolkit.defaults(rn))
+du = copy(u)
+p  = ModelingToolkit.varmap_to_vars(nothing, parameters(rn); defaults=ModelingToolkit.defaults(rn))
+@timeit to "ODE rhs Eval1" oprob.f(du,u,p,0.)
+@timeit to "ODE rhs Eval2" oprob.f(du,u,p,0.)
+sparsejacprob.f(du,u,p,0.)
+
+
+@btime oprob.f($du,$u,$p,0.)
+
+
+sol = solve(oprob, CVODE_BDF(), saveat=tf/1000., reltol=1e-5, abstol=1e-5)
+plot(sol, legend=false, fmt=:png)
+
+
+@time sol = solve(oprob, CVODE_BDF(), abstol=1/10^12, reltol=1/10^12)
+test_sol  = TestSolution(sol)
+
+
+abstols = 1.0 ./ 10.0 .^ (6:10)
+reltols = 1.0 ./ 10.0 .^ (6:10);
+setups = [
+          Dict(:alg=>lsoda()),
+          Dict(:alg=>CVODE_BDF()),
+          Dict(:alg=>TRBDF2()),
+          Dict(:alg=>QNDF()),
+          Dict(:alg=>FBDF()),
+          Dict(:alg=>KenCarp4()),
+          Dict(:alg=>Rosenbrock23()),
+          ];
+
+
+wp = WorkPrecisionSet(oprob,abstols,reltols,setups;error_estimate=:l2,
+                      saveat=tf/10000.,appxsol=test_sol,maxiters=Int(1e5),numruns=100)
+plot(wp)
+
+
+using SciMLBenchmarks
+SciMLBenchmarks.bench_footer(WEAVE_ARGS[:folder],WEAVE_ARGS[:file])
+
