a70e0a03da12781779da9d93076982ee3cf7ba37
SciML Benchmarks CI <buildkite@julialang.org>
2022-09-08 06:59:32 +0000

    Automatic build\nPublished by build of: https://github.com/SciML/SciMLBenchmarks.jl/commit/d3015c14802c052c9f77c3fb48b83a49f472c322

---
+++ script/Bio/fceri_gamma2.jl
@@ -0,0 +1,66 @@
+
+using DiffEqBase, OrdinaryDiffEq, Catalyst, ReactionNetworkImporters,
+      Sundials, Plots, DiffEqDevTools, ODEInterface, ODEInterfaceDiffEq,
+      LSODA, TimerOutputs, LinearAlgebra, ModelingToolkit, BenchmarkTools
+
+gr()
+const to = TimerOutput()
+tf       = 200.0
+
+# generate ModelingToolkit ODEs
+@timeit to "Parse Network" prnbng = loadrxnetwork(BNGNetwork(), joinpath(@__DIR__, "Models/fceri_gamma2.net"))
+rn    = prnbng.rn
+@timeit to "Create ODESys" osys = convert(ODESystem, rn)
+
+tspan = (0.,tf)
+@timeit to "ODEProb No Jac" oprob = ODEProblem(osys, Float64[], tspan, Float64[])
+
+
+@timeit to "ODEProb SparseJac" sparsejacprob = ODEProblem(osys, Float64[], tspan, Float64[], jac=true, sparse=true)
+show(to)
+
+
+@show numspecies(rn) # Number of ODEs
+@show numreactions(rn) # Apprx. number of terms in the ODE
+@show length(parameters(rn)) # Number of Parameters
+
+
+u  = ModelingToolkit.varmap_to_vars(nothing, species(rn); defaults=ModelingToolkit.defaults(rn))
+du = copy(u)
+p  = ModelingToolkit.varmap_to_vars(nothing, parameters(rn); defaults=ModelingToolkit.defaults(rn))
+@timeit to "ODE rhs Eval1" oprob.f(du,u,p,0.)
+@timeit to "ODE rhs Eval2" oprob.f(du,u,p,0.)
+sparsejacprob.f(du,u,p,0.)
+
+
+@btime oprob.f($du,$u,$p,0.)
+
+
+sol = solve(oprob, CVODE_BDF(), saveat=tf/1000., reltol=1e-5, abstol=1e-5)
+plot(sol, legend=false, fmt=:png)
+
+
+@time sol = solve(oprob, CVODE_BDF(), reltol=1e-15, abstol=1e-15)
+test_sol  = TestSolution(sol)
+
+
+abstols = 1.0 ./ 10.0 .^ (5:8)
+reltols = 1.0 ./ 10.0 .^ (5:8);
+setups = [
+          Dict(:alg=>CVODE_BDF(linear_solver=:GMRES)),
+          Dict(:alg=>TRBDF2(linsolve=KrylovJL_GMRES(),autodiff=false)),
+          Dict(:alg=>QNDF(linsolve=KrylovJL_GMRES(),autodiff=false)),
+          Dict(:alg=>FBDF(linsolve=KrylovJL_GMRES(),autodiff=false)),
+          Dict(:alg=>KenCarp4(linsolve=KrylovJL_GMRES(),autodiff=false)),
+          Dict(:alg=>Rosenbrock23(linsolve=KrylovJL_GMRES(),autodiff=false)),
+          ];
+
+
+wp = WorkPrecisionSet(oprob,abstols,reltols,setups;error_estimate=:l2,
+                      saveat=tf/10000.,appxsol=test_sol,maxiters=Int(1e5),numruns=10)
+plot(wp)
+
+
+using SciMLBenchmarks
+SciMLBenchmarks.bench_footer(WEAVE_ARGS[:folder],WEAVE_ARGS[:file])
+
