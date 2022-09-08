dff758beb86d1e0a8ddbd8234e812eaa76027229
SciML Benchmarks CI <buildkite@julialang.org>
2022-09-08 06:57:04 +0000

    Automatic build\nPublished by build of: https://github.com/SciML/SciMLBenchmarks.jl/commit/d3015c14802c052c9f77c3fb48b83a49f472c322

--- script/Bio/multisite2.jl
+++ script/Bio/multisite2.jl
@@ -5,7 +5,7 @@ using DiffEqBase, OrdinaryDiffEq, Catalyst, ReactionNetworkImporters,
 
 gr()
 const to = TimerOutput()
-tf       = 1000.0
+tf       = 2.0
 
 # generate ModelingToolkit ODEs
 @timeit to "Parse Network" prnbng = loadrxnetwork(BNGNetwork(), joinpath(@__DIR__, "Models/multisite2.net"))
@@ -14,6 +14,10 @@ rn    = prnbng.rn
 
 tspan = (0.,tf)
 @timeit to "ODEProb No Jac" oprob = ODEProblem(osys, Float64[], tspan, Float64[])
+
+
+@timeit to "ODEProb SparseJac" sparsejacprob = ODEProblem(osys, Float64[], tspan, Float64[], jac=true, sparse=true)
+show(to)
 
 
 @show numspecies(rn) # Number of ODEs
@@ -36,7 +40,7 @@ sol = solve(oprob, CVODE_BDF(), saveat=tf/1000., reltol=1e-5, abstol=1e-5)
 plot(sol, legend=false, fmt=:png)
 
 
-@time sol = solve(oprob, CVODE_BDF(), abstol=1/10^12, reltol=1/10^12)
+@time sol = solve(oprob, CVODE_BDF(), reltol=1e-15, abstol=1e-15)
 test_sol  = TestSolution(sol)
 
 
@@ -45,17 +49,50 @@ reltols = 1.0 ./ 10.0 .^ (6:10);
 setups = [
           Dict(:alg=>lsoda()),
           Dict(:alg=>CVODE_BDF(linear_solver=:GMRES)),
-          Dict(:alg=>TRBDF2()),
-          Dict(:alg=>QNDF()),
-          Dict(:alg=>FBDF()),
-          Dict(:alg=>KenCarp4()),
-          Dict(:alg=>Rosenbrock23()),
+          Dict(:alg=>TRBDF2(linsolve=KrylovJL_GMRES())),
+          Dict(:alg=>QNDF(linsolve=KrylovJL_GMRES())),
+          Dict(:alg=>FBDF(linsolve=KrylovJL_GMRES())),
+          Dict(:alg=>KenCarp4(linsolve=KrylovJL_GMRES())),
+          Dict(:alg=>Rosenbrock23(linsolve=KrylovJL_GMRES())),
           ];
 
 
 wp = WorkPrecisionSet(oprob,abstols,reltols,setups;error_estimate=:l2,
-                      saveat=tf/10000.,appxsol=test_sol,maxiters=Int(1e5),numruns=100)
+                      saveat=tf/10000.,appxsol=test_sol,maxiters=Int(1e5),numruns=200)
 plot(wp)
+
+
+#Setting BLAS to one thread to measure gains
+LinearAlgebra.BLAS.set_num_threads(1)
+
+abstols = 1.0 ./ 10.0 .^ (10:14)
+reltols = 1.0 ./ 10.0 .^ (7:11)
+
+setups = [
+            Dict(:alg=>CVODE_BDF()),
+            Dict(:alg=>KenCarp4()),
+            Dict(:alg=>Rodas4()),
+            Dict(:alg=>Rodas5()),
+            #Dict(:alg=>QNDF()),
+            Dict(:alg=>lsoda()),
+            Dict(:alg=>radau()),
+            Dict(:alg=>seulex()),
+            Dict(:alg=>ImplicitEulerExtrapolation(threading = OrdinaryDiffEq.PolyesterThreads())),
+            Dict(:alg=>ImplicitEulerExtrapolation(threading = false)),
+            Dict(:alg=>ImplicitEulerBarycentricExtrapolation(threading = OrdinaryDiffEq.PolyesterThreads())),
+            Dict(:alg=>ImplicitEulerBarycentricExtrapolation(threading = false)),
+            Dict(:alg=>ImplicitHairerWannerExtrapolation(threading = OrdinaryDiffEq.PolyesterThreads())),
+            Dict(:alg=>ImplicitHairerWannerExtrapolation(threading = false)),
+            ]
+
+
+solnames = ["CVODE_BDF","KenCarp4","Rodas4","Rodas5","lsoda","radau","seulex","ImplEulerExtpl (threaded)", "ImplEulerExtpl (non-threaded)",
+            "ImplEulerBaryExtpl (threaded)","ImplEulerBaryExtpl (non-threaded)","ImplHWExtpl (threaded)","ImplHWExtpl (non-threaded)"]
+
+plot(wp, title = "Implicit Methods",legend=:outertopleft,size = (1000,500),
+     xticks = 10.0 .^ (-15:1:1),
+     yticks = 10.0 .^ (-6:0.3:5),
+     bottom_margin= 5Plots.mm)
 
 
 using SciMLBenchmarks
