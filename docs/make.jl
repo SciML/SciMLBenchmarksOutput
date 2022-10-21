f8e51ce54703a463941c5679c27aa6f130ea490d
krishna bhogaonker <cyclotomiq@gmail.com>
2022-10-21 15:09:17 -0700

    updated make.jl with new sciml link.

--- docs/make.jl
+++ docs/make.jl
@@ -14,7 +14,7 @@ makedocs(
     clean=true, doctest=false,
     format=Documenter.HTML(#analytics = "UA-90474609-3",
         assets=["assets/favicon.ico"],
-        canonical="https://benchmarks.sciml.ai/stable/"),
+        canonical="https://docs.sciml.ai/SciMLBenchmarksOutput/stable/"),
     pages=pages
 )
 
