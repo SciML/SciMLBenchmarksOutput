1250c58b32ec69956842f9b561c5fcdab4a4aafe
SciML Benchmarks CI <buildkite@julialang.org>
2025-08-27 16:35:57 +0000

    Automatic build\nPublished by build of: https://github.com/SciML/SciMLBenchmarks.jl/commit/1c9d66dad82f17f6b8f7da3c92816240861ad57a

---
+++ script/NonStiffODE/EnrightPryce_wpd.jl
@@ -0,0 +1,142 @@
+
+using OrdinaryDiffEq, ParameterizedFunctions, ODEInterface,
+      ODEInterfaceDiffEq, LSODA, Sundials, DiffEqDevTools,
+      StaticArrays
+using Plots
+gr()
+
+# Load the problems from the Enright-Pryce suite
+include(joinpath(@__DIR__, "enright_pryce.jl"))
+
+abstols = 1.0 ./ 10.0 .^ (6:13)
+reltols = 1.0 ./ 10.0 .^ (3:10)
+
+
+# simdrkalgs = [
+#     Dict(:alg=>MER5v2()),
+#     Dict(:alg=>MER6v2()),
+#     Dict(:alg=>RK6v4())
+# ]
+simdrkalgs = []
+
+
+setups = [
+    Dict(:alg=>Tsit5()),
+    Dict(:alg=>Vern6()),
+    Dict(:alg=>Vern7()),
+    Dict(:alg=>Vern9()),
+    simdrkalgs...
+]
+
+
+prob = NA_PROBLEMS[1]
+test_sol = solve(prob, Vern9(), abstol=1/10^14, reltol=1/10^14)
+wp = WorkPrecisionSet(prob, abstols, reltols, setups; appxsol=test_sol, save_everystep=false, numruns=100)
+plot(wp; title="NA1")
+
+
+prob = NA_PROBLEMS[2]
+test_sol = solve(prob, Vern9(), abstol=1/10^14, reltol=1/10^14)
+wp = WorkPrecisionSet(prob, abstols, reltols, setups; appxsol=test_sol, save_everystep=false, numruns=100)
+plot(wp; title="NA2")
+
+
+prob = NA_PROBLEMS[4]
+test_sol = solve(prob, Vern9(), abstol=1/10^14, reltol=1/10^14)
+wp = WorkPrecisionSet(prob, abstols, reltols, setups; appxsol=test_sol, save_everystep=false, numruns=100)
+plot(wp; title="NA4")
+
+
+prob = NA_PROBLEMS[5]
+test_sol = solve(prob, Vern9(), abstol=1/10^14, reltol=1/10^14)
+wp = WorkPrecisionSet(prob, abstols, reltols, setups; appxsol=test_sol, save_everystep=false, numruns=100)
+plot(wp; title="NA5")
+
+
+prob = NB_PROBLEMS[1]
+test_sol = solve(prob, Vern9(), abstol=1/10^14, reltol=1/10^14)
+wp = WorkPrecisionSet(prob, abstols, reltols, setups; appxsol=test_sol, save_everystep=false, numruns=100)
+plot(wp; title="NB1")
+
+
+prob = NB_PROBLEMS[2]
+test_sol = solve(prob, Vern9(), abstol=1/10^14, reltol=1/10^14)
+wp = WorkPrecisionSet(prob, abstols, reltols, setups; appxsol=test_sol, save_everystep=false, numruns=100)
+plot(wp; title="NB2")
+
+
+prob = NB_PROBLEMS[3]
+test_sol = solve(prob, Vern9(), abstol=1/10^14, reltol=1/10^14)
+wp = WorkPrecisionSet(prob, abstols, reltols, setups; appxsol=test_sol, save_everystep=false, numruns=100)
+plot(wp; title="NB3")
+
+
+prob = NB_PROBLEMS[5]
+test_sol = solve(prob, Vern9(), abstol=1/10^14, reltol=1/10^14)
+wp = WorkPrecisionSet(prob, abstols, reltols, setups; appxsol=test_sol, save_everystep=false, numruns=100)
+plot(wp; title="NB5")
+
+
+prob = NC_PROBLEMS[1]
+test_sol = solve(prob, Vern9(), abstol=1/10^14, reltol=1/10^14)
+wp = WorkPrecisionSet(prob, abstols, reltols, setups; appxsol=test_sol, save_everystep=false, numruns=100)
+plot(wp; title="NC1")
+
+
+prob = NC_PROBLEMS[2]
+test_sol = solve(prob, Vern9(), abstol=1/10^14, reltol=1/10^14)
+wp = WorkPrecisionSet(prob, abstols, reltols, setups; appxsol=test_sol, save_everystep=false, numruns=100)
+plot(wp; title="NC2")
+
+
+prob = NC_PROBLEMS[3]
+test_sol = solve(prob, Vern9(), abstol=1/10^14, reltol=1/10^14)
+wp = WorkPrecisionSet(prob, abstols, reltols, setups; appxsol=test_sol, save_everystep=false, numruns=100)
+plot(wp; title="NC3")
+
+
+prob = NC_PROBLEMS[4]
+test_sol = solve(prob, Vern9(), abstol=1/10^14, reltol=1/10^14)
+wp = WorkPrecisionSet(prob, abstols, reltols, setups; appxsol=test_sol, save_everystep=false, numruns=100)
+plot(wp; title="NC4")
+
+
+prob = NC_PROBLEMS[5]
+test_sol = solve(prob, Vern9(), abstol=1/10^14, reltol=1/10^14)
+wp = WorkPrecisionSet(prob, abstols, reltols, setups; appxsol=test_sol, save_everystep=false, numruns=100)
+plot(wp; title="NC5")
+
+
+prob = ND_PROBLEMS[1]
+test_sol = solve(prob, Vern9(), abstol=1/10^14, reltol=1/10^14)
+wp = WorkPrecisionSet(prob, abstols, reltols, setups; appxsol=test_sol, save_everystep=false, numruns=100)
+plot(wp; title="ND1")
+
+
+prob = ND_PROBLEMS[2]
+test_sol = solve(prob, Vern9(), abstol=1/10^14, reltol=1/10^14)
+wp = WorkPrecisionSet(prob, abstols, reltols, setups; appxsol=test_sol, save_everystep=false, numruns=100)
+plot(wp; title="ND2")
+
+
+prob = ND_PROBLEMS[3]
+test_sol = solve(prob, Vern9(), abstol=1/10^14, reltol=1/10^14)
+wp = WorkPrecisionSet(prob, abstols, reltols, setups; appxsol=test_sol, save_everystep=false, numruns=100)
+plot(wp; title="ND3")
+
+
+prob = ND_PROBLEMS[4]
+test_sol = solve(prob, Vern9(), abstol=1/10^14, reltol=1/10^14)
+wp = WorkPrecisionSet(prob, abstols, reltols, setups; appxsol=test_sol, save_everystep=false, numruns=100)
+plot(wp; title="ND4")
+
+
+prob = ND_PROBLEMS[5]
+test_sol = solve(prob, Vern9(), abstol=1/10^14, reltol=1/10^14)
+wp = WorkPrecisionSet(prob, abstols, reltols, setups; appxsol=test_sol, save_everystep=false, numruns=100)
+plot(wp; title="ND5")
+
+
+using SciMLBenchmarks
+SciMLBenchmarks.bench_footer(WEAVE_ARGS[:folder],WEAVE_ARGS[:file])
+
--- script/NonStiffODE/LotkaVolterra_wpd.jl
+++ script/NonStiffODE/LotkaVolterra_wpd.jl
@@ -3,21 +3,22 @@ using OrdinaryDiffEq, ParameterizedFunctions, ODEInterfaceDiffEq, LSODA,
       Sundials, DiffEqDevTools, StaticArrays
 
 f = @ode_def LotkaVolterra begin
-  dx = a*x - b*x*y
-  dy = -c*y + d*x*y
+    dx = a*x - b*x*y
+    dy = -c*y + d*x*y
 end a b c d
 
-p = SA[1.5,1.0,3.0,1.0]
-prob = ODEProblem{true, SciMLBase.FullSpecialize}(f,[1.0;1.0],(0.0,10.0),p)
-probstatic = ODEProblem{false}(f,SA[1.0;1.0],(0.0,10.0),p)
+p = SA[1.5, 1.0, 3.0, 1.0]
+prob = ODEProblem{true, SciMLBase.FullSpecialize}(f, [1.0; 1.0], (0.0, 10.0), p)
+probstatic = ODEProblem{false}(f, SA[1.0; 1.0], (0.0, 10.0), p)
 
 abstols = 1.0 ./ 10.0 .^ (6:13)
 reltols = 1.0 ./ 10.0 .^ (3:10);
-sol = solve(prob,Vern7(),abstol=1/10^14,reltol=1/10^14)
-sol2 = solve(probstatic,Vern7(),abstol=1/10^14,reltol=1/10^14)
-probs = [prob,probstatic]
-test_sol = [sol,sol2];
-using Plots; gr()
+sol = solve(prob, Vern7(), abstol = 1/10^14, reltol = 1/10^14)
+sol2 = solve(probstatic, Vern7(), abstol = 1/10^14, reltol = 1/10^14)
+probs = [prob, probstatic]
+test_sol = [sol, sol2];
+using Plots;
+gr()
 
 
 plot(sol)
@@ -29,9 +30,9 @@ setups = [Dict(:alg=>DP5())
           Dict(:alg=>Tsit5())
           Dict(:alg=>Vern6())
           Dict(:alg=>Tsit5(), :prob_choice => 2)
-          Dict(:alg=>Vern6(), :prob_choice => 2)
-]
-wp = WorkPrecisionSet(probs,abstols,reltols,setups;appxsol=test_sol,save_everystep=false,maxiters=10000,numruns=100)
+          Dict(:alg=>Vern6(), :prob_choice => 2)]
+wp = WorkPrecisionSet(probs, abstols, reltols, setups; appxsol = test_sol,
+    save_everystep = false, maxiters = 10000, numruns = 100)
 plot(wp)
 
 
@@ -40,9 +41,9 @@ setups = [Dict(:alg=>DP5())
           Dict(:alg=>Tsit5())
           Dict(:alg=>Vern6())
           Dict(:alg=>Tsit5(), :prob_choice => 2)
-          Dict(:alg=>Vern6(), :prob_choice => 2)
-]
-wp = WorkPrecisionSet(probs,abstols,reltols,setups;appxsol=test_sol,maxiters=10000,error_estimate=:L2,dense_errors=true,numruns=100)
+          Dict(:alg=>Vern6(), :prob_choice => 2)]
+wp = WorkPrecisionSet(probs, abstols, reltols, setups; appxsol = test_sol, maxiters = 10000,
+    error_estimate = :L2, dense_errors = true, numruns = 100)
 plot(wp)
 
 
@@ -56,9 +57,9 @@ setups = [Dict(:alg=>DP8())
           Dict(:alg=>Vern6(), :prob_choice => 2)
           Dict(:alg=>Vern7(), :prob_choice => 2)
           Dict(:alg=>Vern8(), :prob_choice => 2)
-          Dict(:alg=>Vern9(), :prob_choice => 2)
-]
-wp = WorkPrecisionSet(probs,abstols,reltols,setups;appxsol=test_sol,save_everystep=false,maxiters=1000,numruns=100)
+          Dict(:alg=>Vern9(), :prob_choice => 2)]
+wp = WorkPrecisionSet(probs, abstols, reltols, setups; appxsol = test_sol,
+    save_everystep = false, maxiters = 1000, numruns = 100)
 plot(wp)
 
 
@@ -74,9 +75,9 @@ setups = [Dict(:alg=>odex())
           Dict(:alg=>Vern9(), :prob_choice => 2)
           Dict(:alg=>CVODE_Adams())
           Dict(:alg=>lsoda())
-          Dict(:alg=>ARKODE(Sundials.Explicit(),order=6))
-          ]
-wp = WorkPrecisionSet(probs,abstols,reltols,setups;appxsol=test_sol,save_everystep=false,maxiters=1000,numruns=100)
+          Dict(:alg=>ARKODE(Sundials.Explicit(), order = 6))]
+wp = WorkPrecisionSet(probs, abstols, reltols, setups; appxsol = test_sol,
+    save_everystep = false, maxiters = 1000, numruns = 100)
 plot(wp)
 
 
@@ -89,9 +90,9 @@ setups = [Dict(:alg=>DP8())
           Dict(:alg=>Vern6(), :prob_choice => 2)
           Dict(:alg=>Vern7(), :prob_choice => 2)
           Dict(:alg=>Vern8(), :prob_choice => 2)
-          Dict(:alg=>Vern9(), :prob_choice => 2)
-]
-wp = WorkPrecisionSet(probs,abstols,reltols,setups;appxsol=test_sol,dense=true,maxiters=1000,error_estimate=:L2,numruns=100)
+          Dict(:alg=>Vern9(), :prob_choice => 2)]
+wp = WorkPrecisionSet(probs, abstols, reltols, setups; appxsol = test_sol, dense = true,
+    maxiters = 1000, error_estimate = :L2, numruns = 100)
 plot(wp)
 
 
@@ -102,37 +103,55 @@ setups = [Dict(:alg=>Tsit5())
           Dict(:alg=>VCABM())
           Dict(:alg=>Vern9(), :prob_choice => 2)
           Dict(:alg=>VCABM(), :prob_choice => 2)
-          Dict(:alg=>AitkenNeville(min_order=1, max_order=9, init_order=4, threading=true))
-          Dict(:alg=>ExtrapolationMidpointDeuflhard(min_order=1, max_order=9, init_order=4, threading=true))
-          Dict(:alg=>ExtrapolationMidpointHairerWanner(min_order=2, max_order=11, init_order=4, threading=true))]
-solnames = ["Tsit5","Vern9","VCABM","Vern9 Static","VCABM Static","AitkenNeville","Midpoint Deuflhard","Midpoint Hairer Wanner"]
-wp = WorkPrecisionSet(probs,abstols,reltols,setups;appxsol=test_sol,names=solnames,
-                      save_everystep=false,verbose=false,numruns=100)
+          Dict(:alg=>AitkenNeville(min_order = 1, max_order = 9, init_order = 4, threading = true))
+          Dict(:alg=>ExtrapolationMidpointDeuflhard(
+              min_order = 1, max_order = 9, init_order = 4, threading = true))
+          Dict(:alg=>ExtrapolationMidpointHairerWanner(
+              min_order = 2, max_order = 11, init_order = 4, threading = true))]
+solnames = ["Tsit5", "Vern9", "VCABM", "Vern9 Static", "VCABM Static",
+    "AitkenNeville", "Midpoint Deuflhard", "Midpoint Hairer Wanner"]
+wp = WorkPrecisionSet(
+    probs, abstols, reltols, setups; appxsol = test_sol, names = solnames,
+    save_everystep = false, verbose = false, numruns = 100)
 plot(wp)
 
 
-setups = [Dict(:alg=>ExtrapolationMidpointDeuflhard(min_order=1, max_order=9, init_order=9, threading=false))
-          Dict(:alg=>ExtrapolationMidpointHairerWanner(min_order=2, max_order=11, init_order=4, threading=false))
-          Dict(:alg=>ExtrapolationMidpointHairerWanner(min_order=2, max_order=11, init_order=4, threading=true))
-          Dict(:alg=>ExtrapolationMidpointHairerWanner(min_order=2, max_order=11, init_order=4, sequence = :romberg, threading=true))
-          Dict(:alg=>ExtrapolationMidpointHairerWanner(min_order=2, max_order=11, init_order=4, sequence = :bulirsch, threading=true))]
-solnames = ["Deuflhard","No threads","standard","Romberg","Bulirsch"]
-wp = WorkPrecisionSet(probs,abstols,reltols,setups;appxsol=test_sol,names=solnames,
-                      save_everystep=false,verbose=false,numruns=100)
+setups = [Dict(:alg=>ExtrapolationMidpointDeuflhard(
+              min_order = 1, max_order = 9, init_order = 9, threading = false))
+          Dict(:alg=>ExtrapolationMidpointHairerWanner(
+              min_order = 2, max_order = 11, init_order = 4, threading = false))
+          Dict(:alg=>ExtrapolationMidpointHairerWanner(
+              min_order = 2, max_order = 11, init_order = 4, threading = true))
+          Dict(:alg=>ExtrapolationMidpointHairerWanner(
+              min_order = 2, max_order = 11, init_order = 4,
+              sequence = :romberg, threading = true))
+          Dict(:alg=>ExtrapolationMidpointHairerWanner(
+              min_order = 2, max_order = 11, init_order = 4,
+              sequence = :bulirsch, threading = true))]
+solnames = ["Deuflhard", "No threads", "standard", "Romberg", "Bulirsch"]
+wp = WorkPrecisionSet(
+    probs, abstols, reltols, setups; appxsol = test_sol, names = solnames,
+    save_everystep = false, verbose = false, numruns = 100)
 plot(wp)
 
 
-setups = [Dict(:alg=>ExtrapolationMidpointHairerWanner(min_order=2, max_order=11, init_order=10, threading=true))
-          Dict(:alg=>ExtrapolationMidpointHairerWanner(min_order=2, max_order=11, init_order=4, threading=true))
-          Dict(:alg=>ExtrapolationMidpointHairerWanner(min_order=5, max_order=11, init_order=10, threading=true))
-          Dict(:alg=>ExtrapolationMidpointHairerWanner(min_order=2, max_order=15, init_order=10, threading=true))
-          Dict(:alg=>ExtrapolationMidpointHairerWanner(min_order=5, max_order=7, init_order=6, threading=true))]
-solnames = ["1","2","3","4","5"]
-wp = WorkPrecisionSet(probs,abstols,reltols,setups;appxsol=test_sol,names=solnames,
-                      save_everystep=false,verbose=false,numruns=100)
+setups = [Dict(:alg=>ExtrapolationMidpointHairerWanner(
+              min_order = 2, max_order = 11, init_order = 10, threading = true))
+          Dict(:alg=>ExtrapolationMidpointHairerWanner(
+              min_order = 2, max_order = 11, init_order = 4, threading = true))
+          Dict(:alg=>ExtrapolationMidpointHairerWanner(
+              min_order = 5, max_order = 11, init_order = 10, threading = true))
+          Dict(:alg=>ExtrapolationMidpointHairerWanner(
+              min_order = 2, max_order = 15, init_order = 10, threading = true))
+          Dict(:alg=>ExtrapolationMidpointHairerWanner(
+              min_order = 5, max_order = 7, init_order = 6, threading = true))]
+solnames = ["1", "2", "3", "4", "5"]
+wp = WorkPrecisionSet(
+    probs, abstols, reltols, setups; appxsol = test_sol, names = solnames,
+    save_everystep = false, verbose = false, numruns = 100)
 plot(wp)
 
 
 using SciMLBenchmarks
-SciMLBenchmarks.bench_footer(WEAVE_ARGS[:folder],WEAVE_ARGS[:file])
+SciMLBenchmarks.bench_footer(WEAVE_ARGS[:folder], WEAVE_ARGS[:file])
 
